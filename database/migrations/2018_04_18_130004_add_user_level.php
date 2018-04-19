c282aa78b0b179641849eb5755f0655f9c66e440
dwi wijonarko <d.wijonarko@gmail.com>
2018-04-19 10:33:40 +0700

    check persmiisons

--- database/migrations/2018_04_18_130004_add_user_level.php
+++ database/migrations/2018_04_18_130004_add_user_level.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddImageColumnToArtikel extends Migration
+class AddUserLevel extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddImageColumnToArtikel extends Migration
      */
     public function up()
     {
-        Schema::table('artikel', function (Blueprint $table) {
-            $table->string('gambar')->after('kategori_id');
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('level');
         });
     }
 
@@ -25,8 +25,8 @@ class AddImageColumnToArtikel extends Migration
      */
     public function down()
     {
-        Schema::table('artikel', function (Blueprint $table) {
-            $table->dropColumn('gambar');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('level');
         });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::middleware(['auth'])->group(function () {
-	Route::resource('/kategori','KategoriController');
-	Route::resource('/user','UserController');	
+	Route::resource('/kategori','KategoriController')->middleware('check-permission:user|admin|superadmin');;
+	Route::resource('/user','UserController')->middleware('check-permission:admin|superadmin');	
 	Route::resource('/artikel','ArtikelController');	
 });
 Route::get('/home', 'HomeController@index')->name('home');
